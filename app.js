2cf22ac9fab2374d316ca9d2dfc749203f5cc367
Heylga <oy.golovina@gmail.com>
2022-01-21 11:27:35 +0000

    first files are created

---
+++ app.js
@@ -0,0 +1,112 @@
+const suits = ["♥", "♦", "♠", "♣"]
+const value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+function getRandomArbitrary(1, 10) {
+    return Math.random(.card-body) * (max - min) + min;
+  }
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class SUITS =["♥", "♦", "♠", "♣"]
+// class VALUES =[
+
+//     "J",
+//     "3",
+//     "8",
+// ]
+
+// get numberOfCards {
+//     return this.cards.length
+// }
+
+// shuffle() {
+//     for (let i = thisnumberOfCards - 1; i>0; i--)
+//     const newIndex = Math.floor(Math.random() * (i + 1))
+//     const oldValue = this.cards[newIndex]
+//     this.cards[newIndex]=this.cards[i]
+//     this.cards[i]= oldValue;
+//     }
+// }
+// }
+
+// class Deck {
+//     constructor() {
+//         this.cards = this.cards
+//     }
+//     shuffle() {
+//         this.card.sort((a, b) => Math.random() - .5)
+//     }
+// }
+
+// class Card {
+//     constructor(suit, value) {
+//         this.suit = suit;
+//         this.value = value;
+//     }
+// }
+
+// function fresh.Deck() {
+//     return SUITS.flatMap(suit => {
+//         return VALUES.map(value => {
+//             return new Card(suit, value)
+//     )}
+//     )}
+// }
+
+
