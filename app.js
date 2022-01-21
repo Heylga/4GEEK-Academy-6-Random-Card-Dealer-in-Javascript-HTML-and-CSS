b593718deae4a86a77dfec1bf8c901af31854ec1
Heylga <oy.golovina@gmail.com>
2022-01-21 16:14:28 +0000

    new variable added in order to unify header&footer suit icons

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const suits = ["♥", "♦", "♠", "♣"];
+const redsuits = ["♥", "♦"];
 const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 let cardbody = document.querySelector(".card-body");
 let cardheader = document.querySelector(".card-header");
 let cardfooter = document.querySelector(".card-footer");
 
 const randomIndex = (arr) => Math.floor(Math.random() * arr.length);
-const getNumber = (arr, i) => arr[i];
-const getSuit = (arr, i) => arr[i];
-
-
+const getRandom = (arr, i) => arr[i];
 
 window.onload = () => { 
-    cardheader.innerHTML = getSuit(suits, randomIndex(suits));
-    cardbody.innerHTML = getNumber(numbers, randomIndex(numbers));
-    cardfooter.innerHTML = getSuit(suits, randomIndex(suits));
+    const icon = getRandom(suits, randomIndex(suits));
+    cardheader.innerHTML = icon;
+    cardbody.innerHTML = getRandom(numbers, randomIndex(numbers));
+    cardfooter.innerHTML = icon;
 };
 
